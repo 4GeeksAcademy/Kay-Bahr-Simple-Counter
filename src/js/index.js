6366700537fb02ed4bc322824b1bc81d11c57d48
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-19 23:08:05 +0000

    counter creation commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Counter from "./component/counter.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
