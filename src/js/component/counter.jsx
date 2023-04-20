02df8d7ad5a1446905ad46409a12579d7daec2e9
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-20 00:01:20 +0000

    counter function commit

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { faClockNine } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Home = () => {
-    
+const Counter = () => {
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds((old) => old + 1);
+        }, 1000);
+        
+        return () => clearInterval(interval);
+    }, [seconds]);
+
+    return (
+        <div>
+            <p><FontAwesomeIcon icon={faClockNine} />{seconds}</p>
+        </div>
+    );
 };
 
 export default Counter;
