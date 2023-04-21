97ebcf59ff72d7755bae36d2048bc8105dc20f34
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-21 08:13:44 +0000

    final commit

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -12,10 +12,37 @@ const Counter = () => {
         return () => clearInterval(interval);
     }, [seconds]);
 
+    const digitStyle = {
+        display: 'inline-block',
+        padding: '10px',
+        paddingTop: '0px',
+        paddingBottom: '0px',
+        border: '2px solid rgb(37, 37, 37)',
+        borderRadius: '5px',
+        margin: '0 5px',
+        marginTop: '10px',
+        marginBottom: '10px',
+        fontSize: '50px',
+        fontFamily: 'Lora',
+        backgroundColor: 'rgb(20, 20, 20)'
+    };
+
+    const clockStyle = {
+        display: 'inline-block',
+        paddingTop: '20px',
+        paddingBottom: '20px',
+        border: '2px solid rgb(37, 37, 37)',
+        borderRadius: '5px',
+        marginRight: '-8px',
+        marginTop: '-15px',
+        marginBottom: '-5px',
+        fontSize: '80px',
+        backgroundColor: 'rgb(20, 20, 20)'
+    };
+
     return (
         <div style={{background: "black"}}>
-            <Clock />
-            <p style={{color: "white"}}>{seconds}</p>
+            <p><Clock style={clockStyle}/> {seconds.toString().padStart(6, '0').split('').map((digit, index) => <span key={index} style={digitStyle}>{digit}{' '}</span>)}</p>
         </div>
     );
 };
